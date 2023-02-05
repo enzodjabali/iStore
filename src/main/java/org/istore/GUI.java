449960138a45341ba3e2c5e9f406d27c02f68361
Enzo <enzo.djabali@supinfo.com>
2023-02-05 14:59:39 +0100

    separating gui classes from SignIn class (#19)

--- src/main/java/org/istore/GUI.java
+++ src/main/java/org/istore/GUI.java
@@ -89,7 +89,7 @@ public class GUI {
             signInButton.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     // this makes sure the button you are pressing is the button variable
-                    if(e.getSource() == signInButton) {
+                    if (e.getSource() == signInButton) {
                         System.out.println(emailField.getText());
                         System.out.println(passwordField.getText());
 
@@ -107,7 +107,7 @@ public class GUI {
                             authFrame.setVisible(false);
 
                             homeFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                            homeFrame.setSize(800,600);
+                            homeFrame.setSize(800, 600);
                             homeFrame.add(new Home());
                             homeFrame.setLocationRelativeTo(null);
                             homeFrame.setVisible(true);
@@ -123,137 +123,138 @@ public class GUI {
                 }
             });
         }
-
-        public class Home extends JPanel {
-            public Home() {
-                //Creating the MenuBar and adding components
-                JMenuBar mb = new JMenuBar();
-                JMenu menuStore = new JMenu("Stores");
-                JMenu menuUsers = new JMenu("Users");
-                mb.add(menuStore);
-                mb.add(menuUsers);
-                JMenuItem menuAccessStore = new JMenuItem("Access a store");
-                JMenuItem menuAddStore = new JMenuItem("Add a store");
-                JMenuItem menuEditStore = new JMenuItem("Edit a store");
-                JMenuItem menuDeleteStore = new JMenuItem("Delete a store");
-                menuStore.add(menuAccessStore);
-                menuStore.add(menuAddStore);
-                menuStore.add(menuEditStore);
-                menuStore.add(menuDeleteStore);
-
-                homeFrame.getContentPane().add(BorderLayout.NORTH, mb);
-
-                // home page here with store listing
-                ArrayList<ArrayList<String>> storeList = listingQueries.getStoreList();
-
-                String header[] = {"ID","Name"};
-
-                String[][] data = new String[storeList.size()][2];
-
-                for (int i = 0; i < storeList.size(); i++) {
-                    data[i] = new String[]{storeList.get(i).get(0), storeList.get(i).get(1)};
-                }
-
-                JTable table = new JTable(data, header);
-                homeFrame.getContentPane().add(new JScrollPane(table));
-                homeFrame.setVisible(true);
-
-
-                // click on access store via menu
-                menuAccessStore.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        if(e.getSource() == menuAccessStore) {
-                            new AccessStore();
-                        }
-                    }
-                });
-
-            }
-        }
-
-
-        public class AccessStore extends JPanel {
-            public AccessStore() {
-                JFrame accessStoreFrame = new JFrame(String.format(dotenv.get("PROJECT_NAME")) + " - Access a store");
-                accessStoreFrame.setSize(350,200);
-                accessStoreFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                accessStoreFrame.setLocationRelativeTo(null);
-                accessStoreFrame.setVisible(true);
-
-                JTextField storeIdField = new JTextField(20);
-                JButton storeAccessButton = new JButton("Access");
-
-                accessStoreFrame.setLayout(new GridBagLayout());
-                GridBagConstraints gbc = new GridBagConstraints();
-
-                gbc.gridx = 0;
-                gbc.gridy = 1;
-                accessStoreFrame.add(new JLabel("Store ID"), gbc);
-
-                gbc.gridy = 0;
-                gbc.gridx = 1;
-                accessStoreFrame.add(new JLabel("<html><h4>Access a store</h4></html>"), gbc);
-
-                gbc.gridx = 1;
-                gbc.gridy = 1;
-                accessStoreFrame.add(storeIdField, gbc);
-
-                gbc.gridy = 3;
-                gbc.gridx = 0;
-                gbc.gridwidth = GridBagConstraints.REMAINDER;
-                accessStoreFrame.add(storeAccessButton, gbc);
-
-                storeAccessButton.addActionListener(new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        if(e.getSource() == storeAccessButton) {
-                            String storeId = storeIdField.getText();
-
-                            if (isNumeric(storeId)) {
-                                int storeIdInt = parseInt(storeId);
-                                accessStoreFrame.dispatchEvent(new WindowEvent(accessStoreFrame, WindowEvent.WINDOW_CLOSING));
-                                new Store(storeIdInt);
-                            } else {
-                                gbc.gridy = 5;
-                                gbc.gridx = 1;
-
-                                accessStoreFrame.add(new JLabel("<html><b style='color: red;'>Please, enter a number!</b></html>"), gbc);
-                                accessStoreFrame.setVisible(true);
-                            }
-
-                        }
-                    }
-                });
-            }
-        }
-
-        public class Store extends JPanel {
-            public Store(int storeId) {
-                JFrame storeFrame = new JFrame(String.format(dotenv.get("PROJECT_NAME")) + " - Store content");
-                storeFrame.setSize(400,500);
-                storeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                storeFrame.setLocationRelativeTo(null);
-                storeFrame.setVisible(true);
-
-                ArrayList<ArrayList<String>> storeList = listingQueries.getStoreItems(storeId);
-
-                String header[] = {"ID", "Name", "Price", "Store"};
-
-                String[][] data = new String[storeList.size()][2];
-
-                for (int i = 0; i < storeList.size(); i++) {
-                    data[i] = new String[] {
-                            storeList.get(i).get(0),
-                            storeList.get(i).get(1),
-                            storeList.get(i).get(2),
-                            storeList.get(i).get(3)
-                    };
-                }
-
-                JTable table = new JTable(data, header);
-                storeFrame.getContentPane().add(new JScrollPane(table));
-                storeFrame.setVisible(true);
-            }
-        }
-
     }
+
+    public class Home extends JPanel {
+        public Home() {
+            //Creating the MenuBar and adding components
+            JMenuBar mb = new JMenuBar();
+            JMenu menuStore = new JMenu("Stores");
+            JMenu menuUsers = new JMenu("Users");
+            mb.add(menuStore);
+            mb.add(menuUsers);
+            JMenuItem menuAccessStore = new JMenuItem("Access a store");
+            JMenuItem menuAddStore = new JMenuItem("Add a store");
+            JMenuItem menuEditStore = new JMenuItem("Edit a store");
+            JMenuItem menuDeleteStore = new JMenuItem("Delete a store");
+            menuStore.add(menuAccessStore);
+            menuStore.add(menuAddStore);
+            menuStore.add(menuEditStore);
+            menuStore.add(menuDeleteStore);
+
+            homeFrame.getContentPane().add(BorderLayout.NORTH, mb);
+
+            // home page here with store listing
+            ArrayList<ArrayList<String>> storeList = listingQueries.getStoreList();
+
+            String header[] = {"ID","Name"};
+
+            String[][] data = new String[storeList.size()][2];
+
+            for (int i = 0; i < storeList.size(); i++) {
+                data[i] = new String[]{storeList.get(i).get(0), storeList.get(i).get(1)};
+            }
+
+            JTable table = new JTable(data, header);
+            homeFrame.getContentPane().add(new JScrollPane(table));
+            homeFrame.setVisible(true);
+
+
+            // click on access store via menu
+            menuAccessStore.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(e.getSource() == menuAccessStore) {
+                        new AccessStore();
+                    }
+                }
+            });
+
+        }
+    }
+
+
+    public class AccessStore extends JPanel {
+        public AccessStore() {
+            JFrame accessStoreFrame = new JFrame(String.format(dotenv.get("PROJECT_NAME")) + " - Access a store");
+            accessStoreFrame.setSize(350,200);
+            accessStoreFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            accessStoreFrame.setLocationRelativeTo(null);
+            accessStoreFrame.setVisible(true);
+
+            JTextField storeIdField = new JTextField(20);
+            JButton storeAccessButton = new JButton("Access");
+
+            accessStoreFrame.setLayout(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            accessStoreFrame.add(new JLabel("Store ID"), gbc);
+
+            gbc.gridy = 0;
+            gbc.gridx = 1;
+            accessStoreFrame.add(new JLabel("<html><h4>Access a store</h4></html>"), gbc);
+
+            gbc.gridx = 1;
+            gbc.gridy = 1;
+            accessStoreFrame.add(storeIdField, gbc);
+
+            gbc.gridy = 3;
+            gbc.gridx = 0;
+            gbc.gridwidth = GridBagConstraints.REMAINDER;
+            accessStoreFrame.add(storeAccessButton, gbc);
+
+            storeAccessButton.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    if(e.getSource() == storeAccessButton) {
+                        String storeId = storeIdField.getText();
+
+                        if (isNumeric(storeId)) {
+                            int storeIdInt = parseInt(storeId);
+                            accessStoreFrame.dispatchEvent(new WindowEvent(accessStoreFrame, WindowEvent.WINDOW_CLOSING));
+                            new Store(storeIdInt);
+                        } else {
+                            gbc.gridy = 5;
+                            gbc.gridx = 1;
+
+                            accessStoreFrame.add(new JLabel("<html><b style='color: red;'>Please, enter a number!</b></html>"), gbc);
+                            accessStoreFrame.setVisible(true);
+                        }
+
+                    }
+                }
+            });
+        }
+    }
+
+    public class Store extends JPanel {
+        public Store(int storeId) {
+            JFrame storeFrame = new JFrame(String.format(dotenv.get("PROJECT_NAME")) + " - Store content");
+            storeFrame.setSize(400,500);
+            storeFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            storeFrame.setLocationRelativeTo(null);
+            storeFrame.setVisible(true);
+
+            ArrayList<ArrayList<String>> storeList = listingQueries.getStoreItems(storeId);
+
+            String header[] = {"ID", "Name", "Price", "Store"};
+
+            String[][] data = new String[storeList.size()][2];
+
+            for (int i = 0; i < storeList.size(); i++) {
+                data[i] = new String[] {
+                        storeList.get(i).get(0),
+                        storeList.get(i).get(1),
+                        storeList.get(i).get(2),
+                        storeList.get(i).get(3)
+                };
+            }
+
+            JTable table = new JTable(data, header);
+            storeFrame.getContentPane().add(new JScrollPane(table));
+            storeFrame.setVisible(true);
+        }
+    }
+
+
 }
